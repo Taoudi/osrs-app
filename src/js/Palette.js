5a47f4494a64545a49964fc1112c9dd3678af2a3
Taoudi <taoudi128@gmail.com>
2020-09-02 15:15:32 +0200

    palette done

--- src/js/Palette.js
+++ src/js/Palette.js
@@ -5,9 +5,6 @@ import Button from 'react-bootstrap/Button';
 class Palette extends Component {
     constructor(props) {
         super(props);
-
-        let map = [{'key': 'RED', 'btn_value':'danger'}, {'key': 'GREY', 'btn_value':'dark'}]
-        this.red = {"color":"red","btn_color":"danger"}
       }
 
 
@@ -19,7 +16,7 @@ class Palette extends Component {
             //<p>{console.log(this.value)}</p>,
             <ul style={{justifyContent: 'center', alignItems: 'center', color:'aliceblue'}}>
             <Button variant="dark" value='#282c34 dark aliceblue' name='colors' onClick={onChangeBtnColor}/>
-            <Button variant="primary" value='darkblue primary aliceblue' onClick={onChangeBtnColor}/>
+            <Button variant="primary" value='steelblue primary aliceblue' onClick={onChangeBtnColor}/>
             <Button variant="info" value='darkcyan info aliceblue' onClick={onChangeBtnColor}/>
             <Button variant="success" value='darkgreen success aliceblue' onClick={onChangeBtnColor}/>
             <Button variant="warning" value='goldenrod warning aliceblue' onClick={onChangeBtnColor}/>
